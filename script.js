f5f5be664f96a819eef85e925595f10b78248f24
=Ashu Sharma <=ashusharma844577@gmail.com>
2025-09-09 17:51:51 +0530

    oprators

---
+++ script.js
@@ -0,0 +1,36 @@
+// const ashu ={
+//     name:"ashu",
+//     uid:1,
+// }
+
+// let u1 =Symbol("uid");
+// ashu[u1]="0001"
+// let condtion =(12>!13)
+
+// let a = condtion?"codition is true":" condtion is false";
+
+
+// let x =20;
+// let y =30;
+
+// if(x>5 && y>40){
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+
+// let islogedIn = true;
+// let isAdmine = false;
+
+// if(isAdmine || islogedIn){
+//      console.log("permission granted")
+// }
+// else{
+//     console.log("permission denied")
+// }
+
+
+
+
+
