67d45043a0e1209c468e35fcf5b97eacf53fbf08
=Ashu Sharma <=ashusharma844577@gmail.com>
2025-09-10 07:56:19 +0530

    oprators

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 
 // let a = condtion?"codition is true":" condtion is false";
 
+//# Practice Qustions
 
 // let x =20;
 // let y =30;
@@ -31,6 +32,42 @@
 // }
 
 
+// let tem = 20;
 
+// if (tem > 30) {
+//     console.log("hot")
+// }
+// else {
+//     console.log("cold");
+// }
+
+// let a =0;
+
+// if(a){
+//     console.log("truthy")
+// }
+// else{
+//     console.log("falshy");
+// }
+
+// Tarnary oprators..
+
+// let marks =50;
+
+// let grade = marks>=90? "A": marks>=50?"B":marks>=35?"pass":"fail";
+
+// console.log(grade);
+
+//! post pre incremet decremet..
+
+// let a =20;
+// a--;
+// console.log(a)
+
+// let x = 3;
+// let y =++x;
+// ++y;
+
+// console.log(x,y)
 
 
