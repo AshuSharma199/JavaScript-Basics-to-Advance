97f4bad1db9285c2f8ac699e5f6784e38ff95a89
ashu sharma <ashusharma844577@gmail.com>
2025-09-11 06:23:18 +0530

    if-else

--- script.js
+++ script.js
@@ -70,4 +70,108 @@
 
 // console.log(x,y)
 
+// console.log(5 + "5");
+
+// console.log("10" - 5);
+// console.log(true + 1);
+
+// let a = 10, b = 20;
+// console.log(a > b && a < b);
+// console.log(a > b || a < b);
+// console.log(!(a === b));
+
+// console.log(`before swapping a=${a} b=${b}`);
+
+// a = a+b;
+// b= a-b;
+// a=a-b;
+// console.log(a,b)
+
+
+// Write a program that checks if a number is positive, negative, or zero.
+// let a = 1;
+
+// if(a>0){
+//     console.log("positive");
+// }
+// else if(a<0){
+//     console.log("negative");
+// }
+// else{
+//     console.log("Zero");
+// }
+
+// Write a program that checks whether a number is even or odd.
+
+// let num = 1;
+
+// if(num%2==0){
+//     console.log("number is even");
+// }
+// else{
+//     console.log("number is odd")
+// }
+
+// Write a program to find the largest of three numbers using if-else.
+
+// let a =10;
+// let b=20;
+// let c=30;
+
+/**if(a>=b && a>=3){
+    console.log(a, " is grater");
+}
+else if(b>=a && b>=c){
+    console.log(b, " is grater");
+}
+else{
+    console.log(c , "is grater")
+}**/
+
+
+// What will be the output?
+
+/**let x = 5;
+if (x = 10) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}**/
+
+// function getGrade(score){
+//   if(score>=90 && score<=100){
+//     return "A+";
+//   }
+//   else if (score>=80 && score<=89){
+//     return "A"
+//   }
+//   else if (score>=70 && score<=79){
+//     return "B"
+//   }
+//   else if(score>=60 && score<=69){
+//     return "C"
+//   }
+//   else if(score>=50 && score<=59){
+//     return "Fail"
+//   }
+//   else {
+//     return "Invalid marks";
+//   }
+// }
+// console.log(getGrade(55))
+
+// function rps(user,computer){
+       
+//   if(user=="rock"&& computer=="sessior") return "user wins";
+//   if(user=="serrior"&& computer =="paper") return "user wins";
+//   if(user=="paper" && computer =="rock") return "user wins";
+
+//   return "computer wins";
+// }
+
+// console.log(rps("serrior" ,"rock" ));
+
+ 
+
+
 
